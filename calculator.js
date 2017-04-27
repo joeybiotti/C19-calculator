f5a5fa80dd53ee7ca79023285517b998e07b7ee4
Joey <joeybb@MacBook-Pro.local>
2017-04-27 15:02:10 -0500

    re-wrote js, attempted to use ES6

--- calculator.js
+++ calculator.js
@@ -1,63 +1,91 @@
+"use-strict"
+
+console.log("calculator.js");
+console.log("output", output);
+
+var num1 = document.getElementById("number1").value;
+var num2 = document.getElementById("number2").value;
+
+
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
  */
-var multply = document.getElementById("multiply").addEventListener("click", function multiplication(event){
-  return(value1 * value2);
-});
+multiply = function(mult){
+  return num1 * num2;
+};
+
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
-var addition = document.getElementById("add").addEventListener("click", function addition(event){
-  return(value1 + value2);
-});
+
+addition = function(add){
+  return num1 + num2;
+};
 
 /*
   Create a function that subtracts two numbers
   passed in as arguments. Return the difference.
  */
-var subtract = document.getElementById("subtract").addEventListener("click", function subtraction(event){
-  return(value1 - value2);
-});
+subtract = function(sub){
+  return num1 - num2;
+}
 
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
-var divide = document.getElementById("divide").addEventListener("click", function division(event){
-  return(value1 - value2);
-});
+
+divide = function(div){
+  return num1 / num2;
+};
 
 
-/*
-  Create a function that accepts three arguments.
-    1. First number
+/* Create a function that accepts three arguments.
+1. First number
     2. Second number
     3. A function that performs an operation on them
 
   Return the value of the operation.
  */
 
-var value1 = document.getElementById("number1").input;
-var value2 = document.getElementById("number2").input;
+var addBtn = document.getElementById("add").addEventListener("click", addition);
+var subBtn = document.getElementById("subtract").addEventListener("click", subtract);
+var multBtn = document.getElementById("multiply").addEventListener("click", multiply);
+var divBtn = document.getElementById("divide").addEventListener("click", divide);
+var equalsBtn = document.getElementById("equals").addEventListener('click', equals);
+var outputArea = document.getElementById("output");
 
-var mathed = function mathItUp(math){
+output = function(math){
   if(addition){
-    return addition
-  } else if(multiply){
-      return(multiply){
-    if(subtract){
-      return(subtract);
-    if(divide){
-      return(divide);
+    return add;
+  }else if(subtract){
+    return sub;
+  }else if(multiply){
+    return mult;
+  }else if(divide){
+    return div;
   }
-};
+}
 
-var output = document.getElementById("output");
 
-console.log("The Answer is ", mathed.value);
-output.innerHTML = "The Answer is ", + mathed.value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
